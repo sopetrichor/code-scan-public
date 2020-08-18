4131f21f21519f905397aec78e3cdc352ea9bc89
Benny Hsieh <stcwer@gmail.com>
2020-08-18 18:46:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const apiKey = "12345678901234567890123456789012-12345678-12345678";
-const db = "abc123.database.secure.windows.net"
+const db = "abc123.database.secure.windows.net";
+const password = "!@#$%^&*()_+";
 
 function endsWith(x, y) {
 //   let index = x.lastIndexOf(y);
