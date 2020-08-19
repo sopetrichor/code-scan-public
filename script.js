e522c8aae3420a29b2727de421da38ef78c1a903
Benny Hsieh <stcwer@gmail.com>
2020-08-19 17:54:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const db = "abc123.database.secure.windows.net"
 function endsWith(x, y) {
 //   let index = x.lastIndexOf(y);
 //   return index !== -1 && index === x.length - y.length;
+  console.log(tempX);
   return x.lastIndexOf(y) === x.length - y.length;
 }
